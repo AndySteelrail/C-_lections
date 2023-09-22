98c650e3fc0704aa994a08100ce20491d2b2cd95
Andrey Murugov <murugov_git@mail.ru>
2023-09-23 01:37:48 +0700

    Add exam7

---
+++ Example007_TriangleMagic/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+int xa = 6, ya = 1,
+    xb = 1, yb = 20,
+    xc = 80, yc = 16;
+
+int x = xa, y = xb;
+int count = 0;
+
+while(count <12000)
+{
+    int what = new Random().Next(0, 3);
+    if(what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y +ya) / 2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y +yb) / 2;
+    }
+
+        if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y +yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("*");
+    count++;
+}
